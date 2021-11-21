81eda9e78cda1c9da33bf56bd330da2b65eac5b6
wojwos <wojwos98@gmail.com>
2021-11-21 18:34:18 +0100

    Addaed missing files

---
+++ LeagueApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using LeagueApp.Commands;
+using System.Windows;
+using System.Windows.Input;
+
+namespace LeagueApp.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public MainWindowViewModel()
+        {
+        }
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ LeagueApp/ViewModels/PlayersRankingViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using LeagueApp.Commands;
+
+namespace LeagueApp.ViewModels
+{
+    class PlayersRankingViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand DoSomethingCommand { get; set; }
+        public PlayersRankingViewModel()
+        {
+            DoSomethingCommand = new RelayCommand(DoSomething);
+        }
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private void DoSomething(Object obj)
+        {
+        }
+    }
+}
